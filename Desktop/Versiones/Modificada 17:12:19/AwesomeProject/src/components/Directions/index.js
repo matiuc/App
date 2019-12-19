9240f817de5b6364e0d04d989841825d0b66f035
Matias Cea <micea@uc.cl>
2019-12-18 23:00:15 -0300

    Remove duplicated directory

--- Desktop/Versiones/Modificada 17:12:19/AwesomeProject/src/components/Directions/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import MapViewDirections from 'react-native-maps-directions'
-
-const Directions = ({destination, origin, }) => <MapViewDirections  />;
-
-export default Directions;
